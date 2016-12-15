c3a9d9bd1c6b3bba8c7ba3d83fe7b3e6d57554ce
sandip <sandipgh19@gmail.com>
2016-12-15 15:54:25 +0530

    update

--- app/src/main/java/com/example/sandip/chatexample/MainActivity.java
+++ app/src/main/java/com/example/sandip/chatexample/MainActivity.java
@@ -83,8 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
                 // Set the child's data to the value passed in from the text box.
                 childRef.setValue(text.getText().toString());
-                String data="";
-                text.setText(data);
+                text.setText("");
 
             }
         });
